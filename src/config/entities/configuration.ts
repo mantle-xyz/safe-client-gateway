85d467f52ebdb1ef18bd2391e75b5426204eb116
Den Smalonski <dzianis.smalonski@protofire.io>
2024-08-01 15:03:34 +0200

    make some env optional

--- src/config/entities/configuration.ts
+++ src/config/entities/configuration.ts
@@ -152,7 +152,7 @@ export default () => ({
   blockchain: {
     blocklist: getBlocklist(),
     infura: {
-      apiKey: process.env.INFURA_API_KEY,
+      apiKey: process.env.INFURA_API_KEY || '',
     },
   },
   bridge: {
@@ -384,12 +384,12 @@ export default () => ({
     baseUri:
       process.env.PUSH_NOTIFICATIONS_API_BASE_URI ||
       'https://fcm.googleapis.com/v1/projects',
-    project: process.env.PUSH_NOTIFICATIONS_API_PROJECT,
+    project: process.env.PUSH_NOTIFICATIONS_API_PROJECT || '',
     serviceAccount: {
       clientEmail:
-        process.env.PUSH_NOTIFICATIONS_API_SERVICE_ACCOUNT_CLIENT_EMAIL,
+        process.env.PUSH_NOTIFICATIONS_API_SERVICE_ACCOUNT_CLIENT_EMAIL || '',
       privateKey:
-        process.env.PUSH_NOTIFICATIONS_API_SERVICE_ACCOUNT_PRIVATE_KEY,
+        process.env.PUSH_NOTIFICATIONS_API_SERVICE_ACCOUNT_PRIVATE_KEY || '',
     },
     getSubscribersBySafeTtlMilliseconds: +(
       process.env.PUSH_NOTIFICATIONS_GET_SUBSCRIBERS_BY_SAFE_TTL_MILLISECONDS ||
